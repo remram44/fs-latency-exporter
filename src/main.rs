eddecca8eaade17bb32bbdbd8df8ca26c20409a4
Remi Rampin <remi@rampin.org>
2024-10-22 23:14:31 -0400

    Add larger buckets

--- src/main.rs
+++ src/main.rs
@@ -86,6 +86,7 @@ Options:
         0.0025, 0.005, 0.01,
         0.025, 0.05, 0.1,
         0.25, 0.5, 1.0,
+        2.5, 5.0, 10.0,
     ]);
     let latency = Histogram::with_opts(latency_opts).unwrap();
     prometheus::default_registry()
